138acd5bca074b0540fd158b4b1e2c4ec32f7006
mahamudulhasan8 <mahamudul.hasan.eng@gmail.com>
2026-09-25 22:32:02 +0600

    Update base.py

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
